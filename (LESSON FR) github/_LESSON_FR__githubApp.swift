d9e16890a59cd46940e709583484078440b6001a
alcaraz728 <alcaraz4@illinois.edu>
2023-06-12 10:02:46 -0500

    Initial Commit

---
+++ (LESSON FR) github/_LESSON_FR__githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  _LESSON_FR__githubApp.swift
+//  (LESSON FR) github
+//
+//  Created by Veronica Alcaraz on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct _LESSON_FR__githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
